351e13d87b093cc06d7f39ce216fb5ee54faa3a6
Carolina Brancher <carito_brancher@hotmail.com>
2022-05-21 15:52:13 -0300

    transportesX

--- Transportesx/backend/routes/api.js
+++ Transportesx/backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require ('cloudinary').v2;
+var nodemailer = require ('nodemailer');
+const async = require('hbs/lib/async');
 
 router.get('/novedades', async function(req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -11,7 +13,7 @@ router.get('/novedades', async function(req, res, next) {
         const imagen = cloudinary.url (novedades.img_id, {
             width:960,
             height:200,
-            crop:'fill' 
+            crop:'pad' 
         });
         return{
           ...novedades, imagen
@@ -24,5 +26,28 @@ router.get('/novedades', async function(req, res, next) {
     }); 
     res.json(novedades);
 });
+
+router.post ('/contacto', async (req, res)=> {
+  const mail = {
+    to: 'carito_brancher@hotmail.com',
+    subject: 'Contacto web',
+    html: `${req.body.nombre} se contacto a traves de la web y quiere más informacion a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> su tel es: ${req.body.telefono}`}
+
+  const transport =nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port:process.env.SMTP_PORT,
+    auth:{
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+      error: false,
+      message:"Mensaje enviado"
+  });
+  });
 
 module.exports=router;
